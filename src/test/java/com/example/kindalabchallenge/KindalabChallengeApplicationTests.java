631c12f40d5bae5e5752fc6a1ac7edd5731c7d87
mpecorari <mpecorari@avaya.com>
2023-03-09 10:43:50 +0000

    first commit

---
+++ src/test/java/com/example/kindalabchallenge/KindalabChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kindalabchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KindalabChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
