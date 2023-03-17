8fac1656b000e42b75482d73171f79815ab7bb1a
mpecorari <mpecorari@avaya.com>
2023-03-17 19:22:00 +0000

    refactor code

--- src/main/java/com/example/kindalabchallenge/ApplicationStartedListener.java
+++ src/main/java/com/example/kindalabchallenge/ApplicationStartedListener.java
@@ -2,17 +2,18 @@ package com.example.kindalabchallenge;
 
 import com.example.kindalabchallenge.model.FreightElevator;
 import com.example.kindalabchallenge.model.PublicElevator;
-import lombok.extern.slf4j.Slf4j;
+import lombok.AllArgsConstructor;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 @Component
-@Slf4j
+@AllArgsConstructor
 public class ApplicationStartedListener implements ApplicationListener<ApplicationStartedEvent> {
+
+    private final ExecutorService executorService;
 
     @Override
     public void onApplicationEvent(ApplicationStartedEvent applicationStartedEvent) {
@@ -20,9 +21,7 @@ public class ApplicationStartedListener implements ApplicationListener<Applicati
     }
 
     private void initiateElevators() {
-        ExecutorService executorService = Executors.newFixedThreadPool(2);
         executorService.submit(PublicElevator.getInstance());
         executorService.submit(FreightElevator.getInstance());
     }
-
 }
